9f574e856768b1aba5684902476537075be49ffb
AddictedtoCloud <92588430+ConnecttheCloud@users.noreply.github.com>
2023-09-06 23:25:28 +0800

    azure-deploy.sh

--- azure-deploy.sh
+++ azure-deploy.sh
@@ -8,6 +8,9 @@ INVENTORY_APP=inventoryapiapp008
 PRODUCT_APP=productapiapp007
 USERIDENTITY=myACRId
 
+echo $(pwd)
+ls -al
+
 az group create -n $RG --location $LOCATION
 
 az acr create \
@@ -64,14 +67,21 @@ az containerapp create \
   --query properties.configuration.ingress.fqdn
 
 # az acr build --registry $ACR_NAME --image $STORE_APP -f ./src/Store/Dockerfile ./src/
+echo "Creating user assigend managed-identity"
 az identity create --resource-group $RG --name $USERIDENTITY
 # Get resource ID of the user-assigned identity
 userID=$(az identity show --resource-group $RG --name $USERIDENTITY --query id --output tsv)
+echo $userID
 # Get service principal ID of the user-assigned identity
 spID=$(az identity show --resource-group $RG --name $USERIDENTITY --query principalId --output tsv)
+echo $spID
 #Get reource ID of ACR
 resourceID=$(az acr show --resource-group $RG --name $ACR_NAME --query id --output tsv)
+echo $resourceID
+
+echo "IAM Role delegation to ACR"
 az role assignment create --assignee $spID --scope $resourceID --role acrpull
+
 az containerapp identity assign --resource-group $RG --name $STORE_APP --user-assigned $userID
 az containerapp identity assign --resource-group $RG --name $INVENTORY_APP --user-assigned $userID
 az containerapp identity assign --resource-group $RG --name $PRODUCT_APP --user-assigned $userID
@@ -95,8 +105,3 @@ az containerapp registry set \
   --resource-group $RG \
   --server $ACR_NAME.azurecr.io \
   --identity $userID
-  # az containerapp registry show \
-  # --name $INVENTORY_APP \
-  # --resource-group $RG \
-  # --server $ACR_NAME.azurecr.io \
-  # --identity $userID
