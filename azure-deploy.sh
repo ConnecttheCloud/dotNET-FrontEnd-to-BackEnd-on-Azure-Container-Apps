0762011c154464f8a783c2145e4ad101046c113e
AddictedtoCloud <92588430+ConnecttheCloud@users.noreply.github.com>
2023-09-06 16:30:23 +0800

    Update azure-deploy.sh

--- azure-deploy.sh
+++ azure-deploy.sh
@@ -6,7 +6,6 @@ ACR_NAME="testdemoacr01"
 WORKSPACE_NAME="workspace10011"
 
 echo $(pwd)
-ls -al
 
 az group create -n $RG --location $LOCATION
 
@@ -14,7 +13,7 @@ az acr create \
   --resource-group $RG \
   --name $ACR_NAME \
   --sku Basic \
-  --admin-enabled true
+  # --admin-enabled true
 
 az monitor log-analytics workspace create \
   --resource-group $RG \
@@ -42,19 +41,29 @@ az containerapp create \
   --registry-server $ACR_NAME.azurecr.io \
   --query properties.configuration.ingress.fqdn
 
+az containerapp create \
+  --name $STORE_APP \
+  --resource-group $RG \
+  --environment $ENVIRONMENT \
+  --image mcr.microsoft.com/azuredocs/containerapps-helloworld:latest \
+  --target-port 80 \
+  --ingress 'external' \
+  --registry-server $ACR_NAME.azurecr.io \
+  --query properties.configuration.ingress.fqdn
+
 # az acr build --registry $ACR_NAME --image $STORE_APP -f ./src/Store/Dockerfile ./src/
-
 az identity create --resource-group $RG --name myACRId
-
 # Get resource ID of the user-assigned identity
 userID=$(az identity show --resource-group $RG --name myACRId --query id --output tsv)
-
 # Get service principal ID of the user-assigned identity
 spID=$(az identity show --resource-group $RG --name myACRId --query principalId --output tsv)
-
 #Get reource ID of ACR
 resourceID=$(az acr show --resource-group $RG --name $ACR_NAME --query id --output tsv)
-
 az role assignment create --assignee $spID --scope $resourceID --role acrpull
-
 az containerapp identity assign --resource-group $RG --name $STORE_APP --user-assigned $userID
+
+az containerapp registry set \
+  --name $STORE_APP \
+  --resource-group $RG \
+  --server $ACR_NAME.azurecr.io \
+  --identity $userID
